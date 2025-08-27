cdae8f3268d6e9b27c53059105cd2c9c640013a1
André Burmeister <andre.b.burmeister@gmail.com>
2025-08-27 14:48:37 -0300

    Make function to play the a single round

--- index.js
+++ index.js
@@ -17,6 +17,26 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  return prompt("Write rock, paper, or scissors:")
+  return prompt("Write rock, paper, or scissors:").toLowerCase()
 }
 
+let humanScore = 0
+let computerScore = 0
+const choiceArr = ['rock', 'paper', 'scissors']
+
+const firstUpper = (s) => s[0].toUpperCase() + s.slice(1)
+
+function playRound(humanChoice, computerChoice) {
+  const choiceDiff = choiceArr.indexOf(humanChoice) - choiceArr.indexOf(computerChoice)
+  console.log(`You picked: ${humanChoice}\nComputer picked: ${computerChoice}`)
+  if (choiceDiff === 0) {
+    console.log(`It's a draw!`)
+    return
+  } else if (choiceDiff === 1 || choiceDiff === -2) {
+    console.log(`${firstUpper(humanChoice)} beats ${computerChoice}! You win!`)
+  } else if (choiceDiff === -1 || choiceDiff === 2) {
+    console.log(`${firstUpper(computerChoice)} beats ${humanChoice}! You loose!`)
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice())
