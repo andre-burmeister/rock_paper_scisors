fe205ac0e5a0f3f0986610e0823cf488dff07ae8
André Burmeister <andre.b.burmeister@gmail.com>
2025-08-27 14:22:19 -0300

    make function that gets a random choice

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+
+function getComputerChoice() {
+  // Solution without arrays:
+  const index = Math.floor(Math.random() * 3)
+  switch (index) {
+    case 0:
+      return "rock"
+    case 1:
+      return "paper"
+    default: // case 2
+      return "scissors"
+  }
+
+  // Solution with arrays:
+  // const index = Math.floor(Math.random() * 3)
+  // return ["rock", "paper", "scissors"][index]
+}
+
