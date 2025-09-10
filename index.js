291d94103cdf93e3e26919ba117016eb22263d59
André Burmeister <andre.b.burmeister@gmail.com>
2025-09-10 10:29:39 -0300

    Make code cleaner and more readable

--- index.js
+++ index.js
@@ -1,63 +1,65 @@
 
 function getComputerChoice() {
   // Solution without arrays:
-  const index = Math.floor(Math.random() * 3)
+  const index = Math.floor(Math.random() * 3);
   switch (index) {
     case 0:
-      return "rock"
+      return "rock";
     case 1:
-      return "paper"
+      return "paper";
     default: // case 2
-      return "scissors"
-  }
+      return "scissors";
+  };
 
   // Solution with arrays:
   // const index = Math.floor(Math.random() * 3)
   // return ["rock", "paper", "scissors"][index]
-}
+};
 
 function getHumanChoice() {
-  return prompt("Write rock, paper, or scissors:").toLowerCase()
-}
+  return prompt("Write rock, paper, or scissors:").toLowerCase();
+};
 
-const firstUpper = (s) => s[0].toUpperCase() + s.slice(1)
+const makeFirstLetterUpper = (s) => s[0].toUpperCase() + s.slice(1);
 
 function playGame() {
 
-  let humanScore = 0
-  let computerScore = 0
-  const choiceArr = ['rock', 'paper', 'scissors']
+  let humanScore = 0;
+  let computerScore = 0;
+  const choiceArr = ['rock', 'paper', 'scissors'];
 
   function playRound(humanChoice, computerChoice) {
-    const choiceDiff = choiceArr.indexOf(humanChoice) - choiceArr.indexOf(computerChoice)
-    console.log(`You picked: ${humanChoice}\nComputer picked: ${computerChoice}`)
-    if (choiceDiff === 0) {
-      console.log(`It's a draw!`)
-      return
-    } else if (choiceDiff === 1 || choiceDiff === -2) {
-      humanScore += 1
-      console.log(`${firstUpper(humanChoice)} beats ${computerChoice}! You win!`)
-    } else if (choiceDiff === -1 || choiceDiff === 2) {
-      computerScore += 1
-      console.log(`${firstUpper(computerChoice)} beats ${humanChoice}! You loose!`)
-    }
-  }
+    const choiceIndDiff = choiceArr.indexOf(humanChoice) - choiceArr.indexOf(computerChoice);
+    console.log(`You picked: ${humanChoice}\nComputer picked: ${computerChoice}`);
+    if (choiceIndDiff === 0) { // Draw
+      console.log(`It's a draw!`);
+      return;
+    } else if (choiceIndDiff === 1 || choiceIndDiff === -2) { // Human wins
+      humanScore += 1;
+      console.log(`${makeFirstLetterUpper(humanChoice)} beats ${computerChoice}! You win!`);
+    } else if (choiceIndDiff === -1 || choiceIndDiff === 2) { // Human loses
+      computerScore += 1;
+      console.log(`${makeFirstLetterUpper(computerChoice)} beats ${humanChoice}! You loose!`);
+    };
+  };
+
   let i = 1;
   while (i<=5 && humanScore < 3 && computerScore < 3) {
-    console.log(`\n\nRound ${i}`)
-    playRound(getHumanChoice(), getComputerChoice())
-    console.log(`Score: you (${humanScore}) x computer (${computerScore})`)
-    i++
-  }
+    console.log(`\n\nRound ${i}`);
+    playRound(getHumanChoice(), getComputerChoice());
+    console.log(`Score: you (${humanScore}) x computer (${computerScore})`);
+    i++;
+  };
+  
   if (humanScore > computerScore) {
-    console.log(`Congratulations! You won!`)
+    console.log(`Congratulations! You won!`);
   } else if (computerScore > humanScore) {
-    console.log("The computer won! You loose!")
+    console.log("The computer won! You loose!");
   } else if (computerScore === humanScore) {
-    console.log("You played five rounds! It's a draw!")
+    console.log("You played five rounds! It's a draw!");
   }
 
-  console.log('\n\n\nTo play again, refresh the page.')
-}
+  console.log('\n\n\nTo play again, refresh the page.');
+};
 
-playGame()
+playGame();
